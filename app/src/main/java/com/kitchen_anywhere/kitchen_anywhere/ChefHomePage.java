93b5c4dd9cd75f64107cd850fe6f7fa72d383e99
Dishant <dishant0170@gmail.com>
2022-04-13 13:34:48 -0400

    add floating action button

--- app/src/main/java/com/kitchen_anywhere/kitchen_anywhere/ChefHomePage.java
+++ app/src/main/java/com/kitchen_anywhere/kitchen_anywhere/ChefHomePage.java
@@ -3,22 +3,33 @@ package com.kitchen_anywhere.kitchen_anywhere;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ChefHomePage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     BottomNavigationView bottomNavigationView;
-
+    FloatingActionButton addDish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_home_page);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        addDish = findViewById(R.id.add_dish);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+        addDish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(ChefHomePage.this,Signup.class)
+            }
+        });
     }
 
     ChefHomePageFragment ChefHomePageFragment = new ChefHomePageFragment();
@@ -42,7 +53,5 @@ public class ChefHomePage extends AppCompatActivity implements BottomNavigationV
         }
         return false;
     }
-
-
 
 }
