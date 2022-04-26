9d8a710fb35add2abc6affefa126fa3df6da97dd
Namra Patel <namra.patel.549@gmail.com>
2022-04-26 14:34:42 -0400

    Firebase get all dish data

--- app/src/main/java/com/kitchen_anywhere/kitchen_anywhere/FoodieHomePage.java
+++ app/src/main/java/com/kitchen_anywhere/kitchen_anywhere/FoodieHomePage.java
@@ -1,4 +1,5 @@
 package com.kitchen_anywhere.kitchen_anywhere;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
@@ -8,11 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.kitchen_anywhere.kitchen_anywhere.model.UserModel;
 
 public class FoodieHomePage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     BottomNavigationView bottomNavigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
