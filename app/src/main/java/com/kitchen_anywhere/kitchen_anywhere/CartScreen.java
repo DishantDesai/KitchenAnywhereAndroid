bc36a5fab58a4342deffb52ff6d1c55918c88c2e
Nency <patelnency37@gmail.com>
2022-05-10 14:04:23 -0400

    wip cart design screen

---
+++ app/src/main/java/com/kitchen_anywhere/kitchen_anywhere/CartScreen.java
@@ -0,0 +1,14 @@
+package com.kitchen_anywhere.kitchen_anywhere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_screen);
+    }
+}
